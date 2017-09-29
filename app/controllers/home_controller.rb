2e5154100c9b05cd057574a2a93c1f87b02aae4b
Doan Minh Phuc <phucdm1901@gmail.com>
2017-09-29 15:02:47 +0000

    review page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @top_rate = Rate.group(:movie).average(:value)
+      .map{|key,val| [key,val.to_f]}.sort_by{|x| -x.last}.shift(8)
+    @top_newest = Movie.order("created_at DESC").limit(8)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'movie/show'
+
   root 'home#index'
+  
+  resources :movie, only: :show
 
   devise_for :users, controllers: {registrations: "users/registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
