6665a17cc4e655332216656cd9b66f4d396447f4
Doan Minh Phuc <phucdm1901@gmail.com>
2017-10-17 15:39:44 +0000

    fix bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def is_admin?
+        if user_signed_in? 
+            return true if current_user.admin == true
+            return false
+        end
+        return false
+    end
 end
